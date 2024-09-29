73e7db5f04d44274e8c2f203d6465e6d0abe707e
Vishwam11 <71768146+Vishwam11@users.noreply.github.com>
2024-09-30 03:44:23 +0530

    Update connect.js

--- Backend/src/config/connect.js
+++ Backend/src/config/connect.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 async function connect() {
   try {
     // console.log(process.env.DB_URI)
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect("mongodb+srv://demo_user:321321@todo.1uspd.mongodb.net/?retryWrites=true&w=majority&appName=todo");
     console.log("Connected to db");
   } catch (err) {
     console.log(err);
